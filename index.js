13f89dbc725023cfb263a4fcbf327d8a6aafadac
vyshu1440 <134669527+vyshu1440@users.noreply.github.com>
2024-09-20 20:30:59 +0530

    Update index.js changed the port number

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.port || 3000;
 
 app.get('/:hash', (req, res) => {
     const urlData = loadURLData();
